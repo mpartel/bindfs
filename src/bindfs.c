6d96d673fa3977364a9cb3c8c9d3c82ced521bb6
Martin Pärtel <martin.partel@gmail.com>
2012-03-13 17:14:21 +0200

    (oops)

--- src/bindfs.c
+++ src/bindfs.c
@@ -1430,7 +1430,7 @@ int main(int argc, char *argv[])
     /* Parse usermap */
     if (od.map) {
         if (getuid() != 0) {
-            fprintf(stderr, "Error: You need to be root to use --map !\n", od.group);
+            fprintf(stderr, "Error: You need to be root to use --map !\n");
             return 1;
         }
         if (!parse_user_map(settings.usermap, settings.usermap_reverse, od.map)) {
