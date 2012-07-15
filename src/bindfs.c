fc2b0700db2ed37ca1f5cbd519e6211f62ed121d
Martin Pärtel <martin.partel@gmail.com>
2012-07-15 12:02:22 +0300

    process_path in readdir

--- src/bindfs.c
+++ src/bindfs.c
@@ -410,6 +410,8 @@ static int bindfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
     int result = 0;
     long pc_ret;
     
+    path = process_path(path);
+    
     pc_ret = pathconf(path, _PC_NAME_MAX);
     if (pc_ret < 0) {
         DPRINTF("pathconf failed: %s (%d)", strerror(errno), errno);
