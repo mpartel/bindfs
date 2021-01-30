591b5217af5537b1c0550643d42f446423c4618c
Martin Pärtel <martin.partel@gmail.com>
2021-01-30 14:41:53 +0200

    Added script test-all.sh

---
+++ test-all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euxo pipefail
+cd "$(dirname "${0}")"
+make
+make check
+sudo make check
+cd tests
+if which valgrind > /dev/null; then
+    ./test_bindfs.rb --valgrind
+    sudo ./test_bindfs.rb --valgrind
+else
+    echo "Valgrind not installed. Not running tests with Valgrind."
+end
